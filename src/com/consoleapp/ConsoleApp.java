d31d8f04d9105f0d89032ecbab82787e82dfa1d5
priyankasamantaroy <priyanka.samantaroy@gmail.com>
2025-11-08 20:48:10 +0000

    completed: folder structure and main method

---
+++ src/com/consoleapp/ConsoleApp.java
@@ -0,0 +1,22 @@
+
+package com.consoleapp;
+
+import java.util.Scanner;
+
+public class ConsoleApp {
+
+	public static void main(String[] args) {		
+		
+				
+			    			
+				// print options to select
+				System.out.println(">> View Products/Catalog :  1");
+				System.out.println(">> Add Items to cart :  2"); 
+				System.out.println(">> Display/View cart items and Itemized total : 3");
+				System.out.println(">> Update/Remove Product: 4");
+				System.out.println(">> Checkout : 5");
+				System.out.println(">> EXIT the Console/System : 6");
+			    
+				
+	}
+}
