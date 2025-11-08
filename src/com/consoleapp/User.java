2bd1395ea00dce2c7312ff28cbe88928922bec42
priyankasamantaroy <priyanka.samantaroy@gmail.com>
2025-11-08 21:38:47 +0000

    Added class: main method - ConsoleApp.java

--- src/com/consoleapp/User.java
+++ src/com/consoleapp/User.java
@@ -1,46 +1,27 @@
 package com.consoleapp;
 
-//java25 Adv OOP: use sealed class to restric inheritence to customer and admin
-public sealed class User permits Customer, Admin {
+public abstract class User {
 
-	protected String name;
-	
+    protected String name;
 
-	public String getName() {
-		return name;
-	}
+    public User() {
+        this.name = "";
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public User(String name) {
+        this.name = name == null ? "" : name;
+    }
 
-	public User(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void displayUserInfo() {
-		//
-	}
+    public void setName(String name) {
+        this.name = name == null ? "" : name;
+    }
 
-	@Override
-	public String toString() {
-		return "User [name=" + name + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "User [name=" + name + "]";
+    }
 }
-
-/*
- abstract class User {
-	 
-	 protected String name;
-	 
-	 public User() {
-		 
-	 }
-
-	void getName(){}
-	 
-	 
-	
-	
-}*/
